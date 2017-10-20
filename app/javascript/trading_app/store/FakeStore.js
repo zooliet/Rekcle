3581eccbdbb5bf4baba348fca435e14991cf7ecf
Junhyun Shin <hl1sqi@gmail.com>
2017-10-20 17:31:34 +0900

    Test with resolved_paths in webpacker.yml

--- app/javascript/trading_app/store/FakeStore.js
+++ app/javascript/trading_app/store/FakeStore.js
@@ -1,19 +1,19 @@
 import { observable, computed, action, autorun } from 'mobx';
-import { fakeCheckUserAPI } from 'trading_app/api/fakeAPI'
+// import { fakeCheckUserAPI } from 'trading_app/api/fakeAPI'
 import faker from 'faker'
 
 class fakeStore {
-  @observable fakeList = {}
-  constructor() {
-    this.fakeList = {
-      title: 'Fake Title'
-    }
-  }
-
-  @action justTestAction(name) {
-    console.log('Test Action fired')
-    this.fakeList.title = name // faker.name.findName()
-  }
+  // @observable fakeList = {}
+  // constructor() {
+  //   this.fakeList = {
+  //     title: 'Fake Title'
+  //   }
+  // }
+  //
+  // @action justTestAction(name) {
+  //   console.log('Test Action fired')
+  //   this.fakeList.title = name // faker.name.findName()
+  // }
 }
 
 export default fakeStore
